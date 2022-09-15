d1a9d67a5a8571bf00ff2b9165998e95a3f89fac
Alexandr000752 <112328813+Alexandr000752@users.noreply.github.com>
2022-09-15 11:16:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int summs = Sum(num);
+Console.WriteLine(summs);
+
+
+
+
+int Sum(int A)
+    {
+        int length = Convert.ToString(A).Length;
+        int resultat = 0;
+            for (int i = 0; i < length; i++)
+            {
+                resultat = resultat + A%10;
+                A = A/10;
+            }
+        return resultat;
+        
+    }
+       
